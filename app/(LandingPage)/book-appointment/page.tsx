4973a0d2f642d393b40bd826b96d967af071eae9
princeobeten <princeobeten56@gmail.com>
2025-07-04 18:41:27 +0100

    Redirect users to external appointment booking page on load in BookAppointmentPage component.

--- app/(LandingPage)/book-appointment/page.tsx
+++ app/(LandingPage)/book-appointment/page.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import { BookAppointmentHeroSection } from './_components/BookAppointmentHeroSection'
 import BookAppointmentFooter from './_components/BookAppointmentFooter'
 import AppointmentBookingFlow from './_components/AppointmentBookingFlow'
 
 const BookAppointmentPage = () => {
-  return (
-    <div className='bg-[#F3FAFD]'>
-      <BookAppointmentHeroSection/>
-      <br />
-      <AppointmentBookingFlow/>
-      <BookAppointmentFooter/>
-    </div>
-  )
+  const router = useRouter();
+  useEffect(() => {
+    window.location.href = 'https://care.ecmsapx.com/appointment/28X74K';
+  }, []);
+  return null;
 }
 
 export default BookAppointmentPage
